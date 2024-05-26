4cb66b798f35df904c510c80b2127d749ca79331
ali <ali_krbl_95@hotmail.com>
2024-05-26 16:08:25 +0300

    first commit

---
+++ vue-site/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import TopMenu from './components/TopMenu.vue'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <TopMenu />
+    </nav>
+  </header>
+
+  <body>
+    <RouterView />
+  </body>
+</template>
+
+<style scoped></style>
